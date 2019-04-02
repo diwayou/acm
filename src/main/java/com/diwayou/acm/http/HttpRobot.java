0816c99c4c7cfa8b0ed4c61878e942bce406d6e8
diwayou <diwayou@qq.com>
2019-04-02 17:58:08 +0800

    http

--- src/main/java/com/diwayou/acm/http/HttpRobot.java
+++ src/main/java/com/diwayou/acm/http/HttpRobot.java
@@ -1,5 +1,6 @@
 package com.diwayou.acm.http;
 
+import com.diwayou.acm.util.StdDraw;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.openqa.selenium.By;
@@ -10,7 +11,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class HttpRobot {
 
@@ -20,27 +25,52 @@ public class HttpRobot {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setHeadless(true);
         WebDriver webDriver = new ChromeDriver(chromeOptions);
-        webDriver.get("http://m.51tiangou.com");
+        StdDraw.setCanvasSize(800, 600);
+        try {
+            webDriver.get("http://m.51tiangou.com");
 
-        WebDriverWait webDriverWait = new WebDriverWait(webDriver, 5);
-        webDriverWait.until(HttpRobot::documentReady);
+            WebDriverWait webDriverWait = new WebDriverWait(webDriver, 5);
+            webDriverWait.until(HttpRobot::documentReady);
 
-        WebElement webElement = webDriver.findElement(By.xpath("/html"));
-        String content = webElement.getAttribute("outerHTML");
+            WebElement webElement = webDriver.findElement(By.xpath("/html"));
+            String content = webElement.getAttribute("outerHTML");
 
-        Document document = Jsoup.parse(content);
 
-        long imageCount = document.select("img").stream()
-                .count();
+            Document document = Jsoup.parse(content);
 
-        System.out.println(imageCount);
+            document.select("img[src]").stream()
+                    .map(e -> e.attr("src"))
+                    .filter(s -> !s.contains("log"))
+                    .forEach(HttpRobot::downloadAndShow);
+        } finally {
+            webDriver.quit();
+        }
+    }
+
+    private static void downloadAndShow(String url) {
+        if (!url.startsWith("http")) {
+            url = "http:" + url;
+        }
+
+        try {
+            BufferedImage image = StdDraw.getImageFast(new URL(url));
+            if (image == null || image.getWidth() < 500 || image.getHeight() < 500) {
+                return;
+            }
+
+            StdDraw.picture(0.2, 0.2, image);
+
+            TimeUnit.SECONDS.sleep(3);
+        } catch (MalformedURLException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     private static boolean documentReady(WebDriver webDriver) {
-        JavascriptExecutor jsExecutor = (JavascriptExecutor)webDriver;
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) webDriver;
         String ready = (String) jsExecutor.executeScript("return document.readyState");
 
-        Boolean imageLoaded = (Boolean)jsExecutor.executeScript("return $('a').length > 2");
+        Boolean imageLoaded = (Boolean) jsExecutor.executeScript("return $('a').length > 2");
 
         return "complete".equalsIgnoreCase(ready) && imageLoaded;
     }
