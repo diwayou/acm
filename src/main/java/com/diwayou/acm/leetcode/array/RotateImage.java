50a94e13cf31209b9fa74f29d57b15185a471b3b
diwayou <diwayou@qq.com>
2019-06-28 18:21:58 +0800

    大礼包

--- src/main/java/com/diwayou/acm/leetcode/array/RotateImage.java
+++ src/main/java/com/diwayou/acm/leetcode/array/RotateImage.java
@@ -1,4 +1,6 @@
-package com.diwayou.acm.leetcode.array;// You are given an n x n 2D matrix representing an image.
+package com.diwayou.acm.leetcode.array;
+
+// You are given an n x n 2D matrix representing an image.
 
 // Rotate the image by 90 degrees (clockwise).
 
---
+++ src/main/java/com/diwayou/acm/team/BigGiftBag.java
@@ -0,0 +1,13 @@
+package com.diwayou.acm.team;
+
+import java.util.List;
+
+/**
+ * 大礼包
+ * https://leetcode-cn.com/problems/shopping-offers/
+ */
+public class BigGiftBag {
+    public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        
+    }
+}
